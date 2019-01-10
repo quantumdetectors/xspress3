3b0fb6e2c0adc2e32501d749d0a5fd2314457910
Stu Fisher <stu@q3f.org>
2019-01-10 10:00:23 +0100

    boot mini iocs when needed

--- iocs/xspress3-ioc.sh
+++ iocs/xspress3-ioc.sh
@@ -4,5 +4,15 @@ if [ -z "$XSPRESS3_CHANNELS" ]; then
     XSPRESS3_CHANNELS=1
 fi
 
+# Use mini specific IOCs
+if [[ $XSPRESS3_MINI -eq 1 ]]; then
+    XSPRESS3_MINI_PREFIX=Mini
+fi
+
+# Can use the default ones for n chans < 3 (single card)
+if [[ $XSPRESS3_CHANNELS -lt 3 && $XSPRESS3_MINI -eq 1 ]]; then
+	XSPRESS3_MINI_PREFIX=
+fi
+
 rm -rf /dev/shm/xsp*
-/usr/lib/epics/iocBoot/xspress3/example${XSPRESS3_CHANNELS}Channel/bin/${EPICS_HOST_ARCH}/stexample${XSPRESS3_CHANNELS}Channel.sh
+/usr/lib/epics/iocBoot/xspress3/example${XSPRESS3_CHANNELS}Channel${XSPRESS3_MINI_PREFIX}/bin/${EPICS_HOST_ARCH}/stexample${XSPRESS3_CHANNELS}Channel.sh
